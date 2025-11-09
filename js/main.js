dd043782227ea53da3f7b62ec8baddd3927c7a43
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-09 13:19:42 +0600

    added sliding banner

--- js/main.js
+++ js/main.js
@@ -11,3 +11,30 @@ navLinks.forEach(link => {
     link.classList.add('active-link');
   });
 });
+
+
+
+const banner = document.getElementById('banner');
+const banners = banner.children;
+let bannerIndex = 0;
+const totalBanners = banners.length;
+
+function showBanner(index) {
+  const shift = index * (100 / banners.length);
+  banner.style.transform = `translateX(-${shift}%)`;
+}
+
+
+document.getElementById('prevBanner').addEventListener('click', () => {
+  bannerIndex = (bannerIndex - 1 + totalBanners) % totalBanners;
+  showBanner(bannerIndex);
+});
+document.getElementById('nextBanner').addEventListener('click', () => {
+  bannerIndex = (bannerIndex + 1) % totalBanners;
+  showBanner(bannerIndex);
+});
+
+setInterval(() => {
+  bannerIndex = (bannerIndex + 1) % totalBanners;
+  showBanner(bannerIndex);
+}, 5000);
