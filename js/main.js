c1746cc6ac000f2504dc1992f06a899230f56eb2
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-09 13:16:50 +0600

    navbar js added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-const productGrid = document.getElementById('productGrid');
-const cartCountSpan = document.getElementById('cartCount');
-let cart = [];
+
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+menuBtn.addEventListener('click', () => mobileMenu.classList.toggle('hidden'));
+
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(l => l.classList.remove('active-link'));
+    link.classList.add('active-link');
+  });
+});
